bd1c2fc1ada6d77914f9093c938f65731572d1a7
Juan van der Breggen <juan@breggen.net>
2017-10-17 22:33:35 +0200

    Updates app installs and docker, fix dataDirs
    Updates to with new individual apps, 
    update docker to docker-ce
    Fix dataDirs function
    Add datadirs.sh test file

---
+++ proxy/proxy_backdoorBankserv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export http_proxy=http://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export HTTP_PROXY=http://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export https_proxy=https://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export HTTPS_PROXY=https://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export ftp_proxy=ftp://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export FTP_PROXY=ftp://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export rsync_proxy=rsync://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export RSYNC_PROXY=rsync://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export socks_proxy=socks://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export SOCKS_PROXY=socks://bankservafrica\\juanv:kr0.Tr3k@172.16.101.93:3128
+export no_proxy=localhost,127.0.0.1,localaddress,.localdomain.com
