cdc3a1f2314ca8a5163d120e351fc48799c69c67
Juan van der Breggen <juan@breggen.net>
2018-07-04 23:21:19 +0200

    Coding menu options, still testing and optimizing

--- test.sh
+++ test.sh
@@ -2,26 +2,27 @@
 
 noPrompt=0
 
-function asking() {
-  if [[ $noPrompt -ne 1 ]]; then
-    read -rp "Do you want to $1? (y/n)" answer
+function asking() { #$1 function to call, $2 Question to ask, $3 Finished message
+  if [[ $noPrompt -eq 0 ]]; then
+    read -rp "Do you want to $2? (y/n)" answer
     if [[ $answer = [Yy1] ]]; then
-      $2
-      pressEnterToContinue "$1"
+      $1
+      pressEnterToContinue "$3"
     fi
   else
-    $2
+    $1
   fi
 }
 
 pressEnterToContinue() {
-  if [[ $noPrompt -ne 1 ]]; then
+  if [[ $noPrompt -eq 0 ]]; then
     read -rp "$1 Press ENTER to continue." nullEntry
     printf "%s" "$nullEntry"
   fi
 }
 
 function menuRun() {
+  local choiceOpt
   until [[ $choiceOpt =~ ^(0|q|Q|quit)$ ]]; do
     clear
     printf "
@@ -42,38 +43,73 @@ function menuRun() {
   done
 }
 
+function mainMenu() {
+  local choiceOpt
+  until [[ $choiceOpt =~ ^(0|q|Q|quit)$ ]]; do
+    clear
+    printf "
+
+    There are the following options for this script
+    TASK : DESCRIPTION
+    -----: ---------------------------------------
+    1    : Select and then auto run
+    2    : Select and run step by step
+    3    : Select from menu and run
+
+    0/q  : Quit this program
+
+    "
+
+    read -rp "Enter your choice : " choiceOpt
+    case $choiceOpt in
+      1|one )
+        menuRun "SelectThenAutoRun"
+      ;;
+      2|two )
+        menuRun "SelectThenStepRun"
+      ;;
+      3|three )
+        menuRun "SelectItem"
+      ;;
+      4|four )
+        menuRun "AutoRun"
+      ;;
+    esac
+  done
+}
+
 function runSelection() {
   # take inputs and perform as necessary
   case $1 in
     1|one )
-      asking "run function one" functionOne
+      asking functionOne "run function one" "Function One Complete"
     ;;
     2|two )
-      asking "run function two" functionTwo
+      asking functionTwo "run Function Two" "Function Two Complete"
     ;;
     3|three )
-      asking "run function three" functionThree
+      asking functionThree "run Function Three" "Function Three Complete"
     ;;
   esac
 }
 
 function stepRun() {
   local stepSelection=()
-  read -rp "Do you want to run one? (y/n)" answer
-  if [[ $answer = [Yy1] ]]; then
-    stepSelection+=("one")
-  fi
-  read -rp "Do you want to run two? (y/n)" answer
-  if [[ $answer = [Yy1] ]]; then
-    stepSelection+=("two")
-  fi
-  read -rp "Do you want to run three? (y/n)" answer
-  if [[ $answer = [Yy1] ]]; then
-    stepSelection+=("three")
-  fi
+
+  function askSteprun() {
+    read -rp "Do you want to run $1? (y/n)" answer
+    if [[ $answer = [Yy1] ]]; then
+      stepSelection+=($2)
+    fi
+  }
+  askSteprun "one" "one"
+  askSteprun "two" "two"
+  askSteprun "three" "three"
+  noPrompt=1
   for i in "${stepSelection[@]}"; do
     runSelection "$i"
   done
+  noPrompt=0
 }
 
 function optionInstall() {
@@ -92,23 +128,6 @@ function functionThree() {
   echo "Running functionThree"
 }
 
-menuRun
-
-asking "asking to run the optionInstall function" optionInstall
-
-firstSteps=(one two three)
-lastSteps=(three two one)
-
-for i in "${firstSteps[@]}"; do
-  runSelection "$i"
-done
-
-for i in "${lastSteps[@]}"; do
-  noPrompt=1
-  runSelection "$i"
-  pressEnterToContinue "Finished lastSteps"
-done
-
-stepRun
+mainMenu
 
 exit 0
