75bcc3b6d298bdceaa2f2a096f339fc76efb6dea
Juan van der Breggen <juan@breggen.net>
2018-08-21 12:03:04 +0200

    Fix RepoUpdate and RepoUpgrades

--- provisioning/vagrantbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-vagrant halt
-vagrant package --base $2 --output /home/juan/VirtualMachines/xVirtualMachines/VirtualBox/vagrantboxes/package.box
-vagrant destroy -f
-vagrant box remove $1 -f
-vagrant box add $1 /home/juan/VirtualMachines/xVirtualMachines/VirtualBox/vagrantboxes/package.box
-rm /home/juan/VirtualMachines/xVirtualMachines/VirtualBox/vagrantboxes/package.box
-rm -r $HOME/.vagrant.d/tmp/vagrant-package-*
