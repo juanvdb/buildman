2347de9026150c4e855e89dc7855fb2e4d797a0c
Juan van der Breggen <juan@breggen.net>
2019-03-18 14:08:00 +0200

    Fix WAF recommendation.

--- vagrant_rsync_guest_to_host.sh
+++ vagrant_rsync_guest_to_host.sh
@@ -2,6 +2,6 @@
 
 rsync -avxP --exclude="lock" --exclude="partial" /var/cache/apt/archives/ juan@172.28.128.1:/data/Backups/vagrantcache/$(lsb_release -cs)/apt/archives/
 
-rsync -avxP juan@172.28.128.1:/data/Backups/vagrantcache/$(lsb_release -cs)/flatpak/repo/refs/remotes/ /var/lib/flatpak/repo/refs/remotes/
+sudo rsync -avxP juan@172.28.128.1:/data/Backups/vagrantcache/$(lsb_release -cs)/flatpak/repo/refs/remotes/ /var/lib/flatpak/repo/refs/remotes/
 
-rsync -avxP /var/lib/snapd/snaps/ juan@172.28.128.1:/data/Backups/vagrantcache/$(lsb_release -cs)/snapd/snaps/
+sudo rsync -avxP /var/lib/snapd/snaps/ juan@172.28.128.1:/data/Backups/vagrantcache/$(lsb_release -cs)/snapd/snaps/
