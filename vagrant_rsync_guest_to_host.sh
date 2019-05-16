e07a79c964431f24a1d0d07c1c47cb5e9fb912d0
Juan van der Breggen <juan@breggen.net>
2019-05-16 12:42:01 +0200

    Update rsyncs not to sync Flatpak

--- vagrant_rsync_guest_to_host.sh
+++ vagrant_rsync_guest_to_host.sh
@@ -3,6 +3,6 @@
 rsync -avxP --exclude="lock" --exclude="partial" /var/cache/apt/archives/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/apt/archives/
 rsync -avxP --exclude="lock" --exclude="partial" /var/cache/apt/lists/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/apt/lists/
 
-sudo rsync -avxP juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/flatpak/repo/refs/remotes/ /var/lib/flatpak/repo/refs/remotes/
+# sudo rsync -avxP /var/lib/flatpak/repo/refs/remotes/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/flatpak/repo/refs/remotes/ 
 
 sudo rsync -avxP /var/lib/snapd/snaps/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/snapd/snaps/
