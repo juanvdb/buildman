d4540310549ebb9816df50f587894fab7502a19c
Juan van der Breggen <juan@breggen.net>
2019-09-09 20:37:26 +0200

    Fix change for task bar

--- kubuntu.buttons.sh
+++ kubuntu.buttons.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-writeconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.lattedock,/Latte,org.kde.LatteDock,activateLauncherMenu"
+# Latte Windows key enable
+kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.lattedock,/Latte,org.kde.LatteDock,activateLauncherMenu"
 qdbus org.kde.KWin /KWin reconfigure
 
-[org.kde.kdecoration2]
-BorderSize=Normal
-ButtonsOnLeft=XIAS
-ButtonsOnRight=H
-CloseOnDoubleClickOnMenu=false
-ShowToolTips=true
-library=org.kde.breeze
-theme=Breeze
+#Titlebar move close, minimize, maximize to left
+kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft "XIAS"
+#Titlebar move help to right
+kwriteconfig5 --file ~/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "H"
+#KDE Tastbar move to top
+kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 2 --key location 3
+kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 7 --key location 3
+#Execute changes
+qdbus org.kde.KWin /KWin reconfigure
