fc4e900f9fc8868d86fec8a8404104b0396afecf
Juan van der Breggen <juan@breggen.net>
2019-09-09 23:34:06 +0200

    Comment out Snap and Flathub

--- vagrant_rsync_guest_to_host.sh
+++ vagrant_rsync_guest_to_host.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 rsync -avxP --exclude="lock" --exclude="partial" /var/cache/apt/archives/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/apt/archives/
-rsync -avxP --exclude="lock" --exclude="partial" /var/cache/apt/lists/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/apt/lists/
+# rsync -avxP --exclude="lock" --exclude="partial" /var/cache/apt/lists/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/apt/lists/
 
-# sudo rsync -avxP /var/lib/flatpak/repo/refs/remotes/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/flatpak/repo/refs/remotes/ 
+# sudo rsync -avxP /var/lib/flatpak/repo/refs/remotes/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/flatpak/repo/refs/remotes/
 
-sudo rsync -avxP /var/lib/snapd/snaps/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/snapd/snaps/
+# sudo rsync -avxP /var/lib/snapd/snaps/ juan@172.28.128.1:/media/juan/xvms/cache/$(lsb_release -cs)/snapd/snaps/
