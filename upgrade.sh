9180509c754e690cdf4093c102d338fc07c05381
Juan van der Breggen <juan@breggen.net>
2020-04-22 08:44:34 +0100

    Updates for rebuild and torrent

--- upgrade.sh
+++ upgrade.sh
@@ -20,15 +20,15 @@ sudo flatpak -y update
 flatpak -y update
 echo -en "\e[7;40;93mSnap Update                                                                       \e[0m\n"
 sudo snap refresh
-if [ -d "/media/juan/xvms/cache" ]; then
-  echo -en "\e[7;40;93mRsync to Vagrant Cache in Backups                                                                 \e[0m\n"
-  # release=$(lsb_release -cs)
-  rsync -avx --exclude="lock" --exclude="partial" /var/cache/apt/archives/ /media/juan/xvms/cache/$(lsb_release -cs)/apt/archives/
-  rsync -avx --exclude="lock" --exclude="partial" /var/lib/apt/lists/ /media/juan/xvms/cache/$(lsb_release -cs)/apt/lists/
-  rsync -avx /var/lib/flatpak/repo/refs/remotes/ /media/juan/xvms/cache/$(lsb_release -cs)/flatpak/repo/refs/remotes/
-  sudo rsync -avx /var/lib/snapd/snaps/ /media/juan/xvms/cache/$(lsb_release -cs)/snapd/snaps
-  sudo chown -R juan:juan /media/juan/xvms/cache/
-fi
+# if [ -d "/media/juan/xvms/cache" ]; then
+#   echo -en "\e[7;40;93mRsync to Vagrant Cache in Backups                                                                 \e[0m\n"
+#   # release=$(lsb_release -cs)
+#   rsync -avx --exclude="lock" --exclude="partial" /var/cache/apt/archives/ /media/juan/xvms/cache/$(lsb_release -cs)/apt/archives/
+#   rsync -avx --exclude="lock" --exclude="partial" /var/lib/apt/lists/ /media/juan/xvms/cache/$(lsb_release -cs)/apt/lists/
+#   rsync -avx /var/lib/flatpak/repo/refs/remotes/ /media/juan/xvms/cache/$(lsb_release -cs)/flatpak/repo/refs/remotes/
+#   sudo rsync -avx /var/lib/snapd/snaps/ /media/juan/xvms/cache/$(lsb_release -cs)/snapd/snaps
+#   sudo chown -R juan:juan /media/juan/xvms/cache/
+# fi
 echo -en "\e[7;40;93mApt Autoremove                                                                    \e[0m\n"
 sudo apt -y autoremove
 echo -en "\e[7;40;93mApt Clean                                                                         \e[0m\n"
