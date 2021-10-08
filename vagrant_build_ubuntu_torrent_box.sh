d15c36261c8e55f0357ddab79491d2e01eb4921e
Juan van der Breggen <juan@breggen.net>
2021-10-08 17:42:24 +0100

    Fixed unzip to be quiet.

--- vagrant_build_ubuntu_torrent_box.sh
+++ vagrant_build_ubuntu_torrent_box.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 sudo apt install -y wget qbittorrent
 sudo wget -qnc -P "$HOME/tmp" https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-sudo dpkg -iy "$HOME/tmp/nordvpn-release_1.0.0_all.deb"
+sudo dpkg -i "$HOME/tmp/nordvpn-release_1.0.0_all.deb"
 sudo apt update
 sudo apt -y install nordvpn
+sudo usermod -aG nordvpn $USER
 nordvpn login
 nordvpn whitelist add port 22
 nordvpn whitelist add subnet 172.28.128.1/24
@@ -16,7 +17,7 @@ sudo apt install -y openvpn network-manager-openvpn ca-certificates
 nowpath=$(pwd)
 cd /etc/openvpn || exit
 sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
-sudo unzip ovpn.zip
+sudo unzip -q ovpn.zip
 sudo rm ovpn.zip
 cd "$nowpath" || exit
 sudo service network-manager restart
